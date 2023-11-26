f3410bdb02e8375bb3d72eb8242f6b2e3b65678e
Ashiamd <ashiamd@foxmail.com>
2023-11-27 02:46:53 +0800

    2.1 生成一个类

---
+++ bytebuddy_test/src/test/java/org/example/NothingClass.java
@@ -0,0 +1,10 @@
+package org.example;
+
+/**
+ * 字面意, 无任何自定义代码的空实现类, 仅用于代码测试
+ *
+ * @author : Ashiamd email: ashiamd@foxmail.com
+ * @date : 2023/11/26 11:53 PM
+ */
+public class NothingClass {
+}
